fd317e43468cf9daa77fad1fff0787e5a791e596
IbrahimMohammed47 <ibrahim.mohammed.4748@gmail.com>
2021-03-22 00:11:17 +0200

    added all jumia countries

--- injector.js
+++ injector.js
@@ -6,21 +6,23 @@
   warningImgUrl = chrome.runtime.getURL("boycot.jpeg");
 
   injectScript('helpers.js')
-  let currURL = location.href;
-  let script;
-  if (currURL.includes("jumia.com")){
-    script=injectScript('jumia-blocker.js')
-  }
-  else if (currURL.includes("souq.com")){
-    script=injectScript('souq-blocker.js')
-  }
-  script.onload = function() {
-    this.remove();  
-    var event = new CustomEvent('SendJsonUrl', {
-      detail: {jsonObj,warningImgUrl}
-    })
-    document.dispatchEvent(event);
-  };
+  setTimeout(() => {
+    let currURL = location.href;
+    let script;
+    if (currURL.includes("jumia.com")){
+      script=injectScript('jumia-blocker.js')
+    }
+    else if (currURL.includes("souq.com")){
+      script=injectScript('souq-blocker.js')
+    }
+    script.onload = function() {
+      this.remove();  
+      var event = new CustomEvent('SendJsonUrl', {
+        detail: {jsonObj,warningImgUrl}
+      })
+      document.dispatchEvent(event);
+    };
+  }, .1);
 })()
 
 function injectScript(scriptName){
